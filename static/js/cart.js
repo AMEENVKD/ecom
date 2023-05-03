26175bcbdc5d374bf0cb25fce134cdba1548f3a1
Ameen Ibrahim <nuamhan@gmail.com>
2023-05-03 19:22:07 +0530

    commit

---
+++ static/js/cart.js
@@ -0,0 +1,43 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+
+for(var i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:',productId, 'action',action)
+
+        console.log('USER:', user)
+        if (user === 'Anonymoususer'){
+            console.log('User is  authenticated')
+        }else{
+           console.log('User is authenticated')
+        }
+    })
+
+}
+
+function updateUserOrder(productId,action){
+    console.log('User is logged in, sendingdata...')
+
+    var url = 'update_item'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+            
+        },
+        body:JSON.stringify({'productId' : productId,'action:':action})
+    })
+
+    .then((response) =>{
+        return response.json()
+    })
+
+    .then((data) =>{
+        console.log('data:',data)
+    })
+}
+
